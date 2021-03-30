9ae392283217c72dba436d30022eb9025251d9d1
Alexey Zhokhov <alexey@zhokhov.com>
2021-03-31 06:17:27 +0800

    Improved check-dependency-updates.sh.

--- check-dependency-updates.sh
+++ check-dependency-updates.sh
@@ -4,4 +4,4 @@ cd "${ABSOLUTE_PATH}" || exit
 
 set -e
 
-./gradlew dependencyUpdates -Drevision=release --info ${GRADLE_EXTRA_ARGS}
+./gradlew dependencyUpdates -Drevision=release --info --parallel --continue --no-daemon ${GRADLE_EXTRA_ARGS}

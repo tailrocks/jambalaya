27c90863aac7e4757b5c427810d447eb8b4731a7
Alexey Zhokhov <alexey@zhokhov.com>
2022-11-27 01:03:00 +0800

    Build fix.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,21 @@
 plugins {
     // https://plugins.gradle.org/plugin/com.tailrocks.java
-    id("com.tailrocks.java") version "0.1.4"
+    id("com.tailrocks.java") version "0.2.0"
 
     // https://plugins.gradle.org/plugin/com.tailrocks.spotless
-    id("com.tailrocks.spotless") version "0.1.4"
+    id("com.tailrocks.spotless") version "0.2.0"
 
     // https://plugins.gradle.org/plugin/com.tailrocks.idea
-    id("com.tailrocks.idea") version "0.1.3" apply false
+    id("com.tailrocks.idea") version "0.2.1" apply false
 
     // https://plugins.gradle.org/plugin/com.tailrocks.junit
-    id("com.tailrocks.junit") version "0.1.5" apply false
+    id("com.tailrocks.junit") version "0.2.0" apply false
 
     // https://plugins.gradle.org/plugin/com.tailrocks.versions
-    id("com.tailrocks.versions") version "0.1.4" apply false
+    id("com.tailrocks.versions") version "0.2.0" apply false
 
     // https://plugins.gradle.org/plugin/com.tailrocks.kotlin
-    id("com.tailrocks.kotlin") version "0.1.4" apply false
+    id("com.tailrocks.kotlin") version "0.2.0" apply false
 
     // https://plugins.gradle.org/plugin/com.adarshr.test-logger
     id("com.adarshr.test-logger") version "3.2.0" apply false

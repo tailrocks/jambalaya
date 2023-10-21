c285b130f034b7b3e6b515cbe76d8ed53a3c664b
Alexey Zhokhov <alexey@zhokhov.com>
2023-10-21 22:44:41 +0800

    Up gradle plugins.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,24 @@
 plugins {
     // https://plugins.gradle.org/plugin/com.tailrocks.java
-    id("com.tailrocks.java") version "0.2.0"
+    id("com.tailrocks.java") version "0.3.0"
 
     // https://plugins.gradle.org/plugin/com.tailrocks.spotless
-    id("com.tailrocks.spotless") version "0.2.0"
+    id("com.tailrocks.spotless") version "0.3.0"
 
     // https://plugins.gradle.org/plugin/com.tailrocks.idea
-    id("com.tailrocks.idea") version "0.2.1" apply false
+    id("com.tailrocks.idea") version "0.3.0" apply false
 
     // https://plugins.gradle.org/plugin/com.tailrocks.junit
-    id("com.tailrocks.junit") version "0.2.0" apply false
+    id("com.tailrocks.junit") version "0.3.0" apply false
 
     // https://plugins.gradle.org/plugin/com.tailrocks.versions
-    id("com.tailrocks.versions") version "0.2.0" apply false
+    id("com.tailrocks.versions") version "0.3.0" apply false
 
     // https://plugins.gradle.org/plugin/com.tailrocks.kotlin
-    id("com.tailrocks.kotlin") version "0.2.0" apply false
+    id("com.tailrocks.kotlin") version "0.3.0" apply false
 
     // https://plugins.gradle.org/plugin/com.adarshr.test-logger
-    id("com.adarshr.test-logger") version "3.2.0" apply false
+    id("com.adarshr.test-logger") version "4.0.0" apply false
 
     // https://plugins.gradle.org/plugin/com.google.protobuf
     id("com.google.protobuf") version "0.9.4" apply false

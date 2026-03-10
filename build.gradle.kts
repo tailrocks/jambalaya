52fea4a2aa05a4481a4f73d000fdd2b1aa001c7f
Alexey Zhokhov <alexey@zhokhov.com>
2026-03-10 11:54:40 +0700

    Add nmcp aggregation plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ plugins {
     // https://plugins.gradle.org/plugin/com.google.protobuf
     id("com.google.protobuf") version "0.9.6" apply false
 
+    // https://github.com/GradleUp/nmcp
     id("com.gradleup.nmcp.aggregation") version "1.4.4"
 }
 
